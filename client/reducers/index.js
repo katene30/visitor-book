4a2d79e92c2d2731cb5d88722b3f2fef6f876b6c
Katene Jack Cotton <katenejack@gmail.com>
2019-04-25 18:29:43 +1200

    clear all todo stuff

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,5 @@
 import {combineReducers} from 'redux'
 
-import todos from './todos'
-import showTodo from './showTodo'
-import categories from './categories'
 import logs from './logs'
 
 const reducers = combineReducers({
