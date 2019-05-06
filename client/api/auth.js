8d534194c426ec57c7955867817fc454ec0e30f0
Katene Jack Cotton <katenejack@gmail.com>
2019-05-06 16:45:30 +1200

    setup for login

--- client/api/auth.js
+++
@@ -1,9 +0,0 @@
-import request from 'superagent'
-const apiEndpoint = ("https://mashape-community-urban-dictionary.p.mashape.com/define?term=")
-
-export function getDefinition(name) {
-  return request 
-  .get(apiEndpoint + name).set('x-mashape-key', 'IUU7GyrOK4mshNOCbJEmBmj5SKSop1kSmPJjsn2nWIxgxzZbEc').then(res => {
-    return res.body.list[0]
-  })
-}
